c34c800ba9290257392a991a8c2201c77025843e
Himesh Kandpal <asuskandpal@gmail.com>
2025-12-28 20:58:20 +0530

    add coding

--- Project/Hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Content-Type: text/html\n\n");
-    printf("<html><body>");
-    printf("<h1>Hello from C Backd!</h1>");
-    printf("</body></html>");
-    return 0;
-}
