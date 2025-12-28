055772ee3dbf93379ad7c5f89cec452070d10a25
Himesh Kandpal <asuskandpal@gmail.com>
2025-12-28 18:38:02 +0530

    Add files via upload

---
+++ Project/Hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Content-Type: text/html\n\n");
+    printf("<html><body>");
+    printf("<h1>Hello from C Backd!</h1>");
+    printf("</body></html>");
+    return 0;
+}
