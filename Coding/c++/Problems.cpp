9f1b560392c6034867cc0f19bc0fb3e009283077
Himesh Kandpal <asuskandpal@gmail.com>
2026-01-17 19:37:32 +0530

    problem.cpp

---
+++ Coding/c++/Problems.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+
+    string Tea = "LemonTea";
+    float LemonTea_Price = 20.99;
+    int Price_Per_KG = 10;
+    char rating = 'A';
+
+    cout<<endl<<"---The Tea Prices With Rating---"<<endl;
+    cout<<endl;
+    
+    cout<<"Tea Name : "<<Tea<<endl;
+    cout<<"Lemon Tea price : "<<LemonTea_Price<<endl;
+    cout<<"Price per kg : "<<Price_Per_KG<<endl;
+    cout<<"Rating : "<<rating<<endl;
+
+    return 0;
+}
